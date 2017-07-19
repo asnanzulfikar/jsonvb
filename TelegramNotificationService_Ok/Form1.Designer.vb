29733ff366f43297fbd8059c4e0469e978b85eb5
surv1v0r <asnan_zulfikar@ymail.com>
2017-07-20 03:49:02 +0700

    versi 1.0.0

---
+++ TelegramNotificationService_Ok/Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btrefresh = New System.Windows.Forms.Button()
+        Me.btdisconnect = New System.Windows.Forms.Button()
+        Me.btconnect = New System.Windows.Forms.Button()
+        Me.cbbaudrate = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.cbport = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btrefresh
+        '
+        Me.btrefresh.Location = New System.Drawing.Point(268, 17)
+        Me.btrefresh.Name = "btrefresh"
+        Me.btrefresh.Size = New System.Drawing.Size(121, 23)
+        Me.btrefresh.TabIndex = 10
+        Me.btrefresh.Text = "Refresh Port"
+        Me.btrefresh.UseVisualStyleBackColor = True
+        '
+        'btdisconnect
+        '
+        Me.btdisconnect.Location = New System.Drawing.Point(93, 120)
+        Me.btdisconnect.Name = "btdisconnect"
+        Me.btdisconnect.Size = New System.Drawing.Size(121, 23)
+        Me.btdisconnect.TabIndex = 9
+        Me.btdisconnect.Text = "Disconnect"
+        Me.btdisconnect.UseVisualStyleBackColor = True
+        '
+        'btconnect
+        '
+        Me.btconnect.Location = New System.Drawing.Point(93, 87)
+        Me.btconnect.Name = "btconnect"
+        Me.btconnect.Size = New System.Drawing.Size(121, 23)
+        Me.btconnect.TabIndex = 5
+        Me.btconnect.Text = "Connect"
+        Me.btconnect.UseVisualStyleBackColor = True
+        '
+        'cbbaudrate
+        '
+        Me.cbbaudrate.FormattingEnabled = True
+        Me.cbbaudrate.Location = New System.Drawing.Point(93, 55)
+        Me.cbbaudrate.Name = "cbbaudrate"
+        Me.cbbaudrate.Size = New System.Drawing.Size(121, 21)
+        Me.cbbaudrate.TabIndex = 8
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btrefresh)
+        Me.GroupBox1.Controls.Add(Me.btdisconnect)
+        Me.GroupBox1.Controls.Add(Me.btconnect)
+        Me.GroupBox1.Controls.Add(Me.cbbaudrate)
+        Me.GroupBox1.Controls.Add(Me.cbport)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(409, 148)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "COM PORT SELECT"
+        '
+        'cbport
+        '
+        Me.cbport.FormattingEnabled = True
+        Me.cbport.Location = New System.Drawing.Point(93, 19)
+        Me.cbport.Name = "cbport"
+        Me.cbport.Size = New System.Drawing.Size(121, 21)
+        Me.cbport.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Baud Rate"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "COM"
+        '
+        'Timer1
+        '
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.RichTextBox2)
+        Me.GroupBox3.Location = New System.Drawing.Point(427, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(604, 143)
+        Me.GroupBox3.TabIndex = 9
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Json Debug"
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(6, 15)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.ReadOnly = True
+        Me.RichTextBox2.Size = New System.Drawing.Size(592, 133)
+        Me.RichTextBox2.TabIndex = 1
+        Me.RichTextBox2.Text = ""
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(429, 166)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(84, 20)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "db status"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 166)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(54, 18)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        Me.RichTextBox1.Visible = False
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(12, 190)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(1019, 420)
+        Me.WebBrowser1.TabIndex = 6
+        Me.WebBrowser1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1043, 613)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Name = "Form1"
+        Me.Text = "Telegram Notification Service"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btrefresh As System.Windows.Forms.Button
+    Friend WithEvents btdisconnect As System.Windows.Forms.Button
+    Friend WithEvents btconnect As System.Windows.Forms.Button
+    Friend WithEvents cbbaudrate As System.Windows.Forms.ComboBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents cbport As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents RichTextBox2 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+
+End Class
---
+++ TelegramNotificationService_Ok/Form1.vb
@@ -0,0 +1,186 @@
+﻿Imports System.IO
+Imports System.IO.Ports
+Imports System.Threading
+Imports MySql.Data.MySqlClient
+Public Class Form1
+    Dim conn As New MySqlConnection
+    Dim serdata As String
+    Dim vartoken As String
+    Dim varchatid As String
+    Dim tempport As String
+    Dim jsondata As String
+    Dim cmd As New MySqlCommand
+    Dim DR As MySqlDataReader
+    Dim DatabaseName As String = "db_telegram"
+    Dim server As String = "127.0.0.1"
+    Dim userName As String = "root"
+    Dim password As String = ""
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        cbbaudrate.Items.Add(9600)
+        cbbaudrate.Items.Add(19200)
+        cbbaudrate.Items.Add(38400)
+        cbbaudrate.Items.Add(57600)
+        cbbaudrate.Items.Add(115200)
+        cbportfill()
+        btdisconnect.Enabled = False
+        connect()
+    End Sub
+
+    Private Sub btconnect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btconnect.Click
+        If cbport.Text = "" Then
+            MessageBox.Show("You must select port first..")
+        ElseIf cbbaudrate.Text = "" Then
+            MessageBox.Show("You must select baud rate first..")
+        Else
+            vartoken = "369348634:AAFQsPBjnmNziAMs00CwKbPfE00igQ4h260"
+            varchatid = "248735818"
+            tempport = cbport.Text
+            cbport.Items.Clear()
+            cbportfill()
+            If cbport.Items.Contains(tempport) Then
+                'Do nothing
+                cbport.Enabled = False
+                cbbaudrate.Enabled = False
+                SerialPort1.PortName = cbport.Text
+                SerialPort1.BaudRate = cbbaudrate.Text
+                btconnect.Enabled = False
+                SerialPort1.Open()
+                btrefresh.Enabled = False
+                btdisconnect.Enabled = True
+                Timer1.Enabled = True
+            Else
+                MessageBox.Show("Port " & tempport & " unavailable")
+                cbport.Text = ""
+                cbport.Items.Clear()
+            End If
+        End If
+    End Sub
+
+    Private Sub btdisconnect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btdisconnect.Click
+        btdisconnect.Enabled = False
+        Timer1.Enabled = False
+        SerialPort1.Close()
+        btconnect.Enabled = True
+        btrefresh.Enabled = True
+        cbport.Enabled = True
+        cbbaudrate.Enabled = True
+        RichTextBox1.Text = ""
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If SerialPort1.IsOpen Then
+            'SerialPort1.Write(Chr(1))
+            serdata = SerialPort1.ReadExisting
+            RichTextBox1.Text = serdata
+            If RichTextBox1.Text = "" Then
+                'Do nothing here
+            Else
+                Dim req As Net.HttpWebRequest = Net.WebRequest.Create("https://api.telegram.org/bot" & vartoken & "/sendMessage?chat_id=" & varchatid & "&text=" & serdata)
+
+                Dim response As Net.WebResponse = req.GetResponse
+
+                Dim stream As System.IO.Stream = response.GetResponseStream
+
+                'Prepare buffer for reading from stream
+                Dim buffer As Byte() = New Byte(1000) {}
+
+                'Data read from stream is gathered here
+                Dim data As New List(Of Byte)
+
+                'Start reading stream
+                Dim bytesRead = stream.Read(buffer, 0, buffer.Length)
+
+                Do Until bytesRead = 0
+                    For i = 0 To bytesRead - 1
+                        data.Add(buffer(i))
+                    Next
+
+                    bytesRead = stream.Read(buffer, 0, buffer.Length)
+                Loop
+
+                'Gets the JSON data
+                'Debug.WriteLine(System.Text.Encoding.UTF8.GetString(data.ToArray))
+                jsondata = System.Text.Encoding.UTF8.GetString(data.ToArray)
+                RichTextBox2.Text = jsondata
+                filljsontodb()
+                response.Close()
+                stream.Close()
+                WebBrowser1.Navigate("http://localhost/telegram/page/home.php")
+                WebBrowser1.ScrollBarsEnabled = False
+                WebBrowser1.Show()
+            End If
+        Else
+            Application.Exit()
+        End If
+    End Sub
+
+    Private Sub btrefresh_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btrefresh.Click
+        refreshform()
+    End Sub
+
+    Public Sub cbportfill()
+        For i As Integer = 0 To My.Computer.Ports.SerialPortNames.Count - 1
+            cbport.Items.Add(My.Computer.Ports.SerialPortNames(i))
+        Next
+    End Sub
+
+    Public Sub refreshform()
+        cbport.Text = ""
+        cbport.Items.Clear()
+        cbportfill()
+        btdisconnect.Enabled = False
+    End Sub
+
+    Public Sub connect()
+        Dim DatabaseName As String = "db_telegram"
+        Dim server As String = "127.0.0.1"
+        Dim userName As String = "root"
+        Dim password As String = ""
+        If Not conn Is Nothing Then conn.Close()
+        conn.ConnectionString = String.Format("server={0}; user id={1}; password={2}; database={3}; pooling=false", server, userName, password, DatabaseName)
+        Try
+            conn.Open()
+            Label5.ForeColor = Color.Green
+            Label5.Text = "Sucsessfully connected to database"
+        Catch ex As Exception
+            Label5.ForeColor = Color.Red
+            Label5.Text = "Warning : " & ex.Message
+        End Try
+        conn.Close()
+    End Sub
+
+    Public Sub filljsontodb()
+        conn.Open()
+        cmd.Connection = conn
+        cmd.CommandText = "SELECT * FROM `json_data` WHERE 1"
+        DR = cmd.ExecuteReader
+        If DR.HasRows Then
+            cmd.CommandText = "DELETE FROM `json_data` WHERE 1"
+            DR.Close()
+            Try
+                cmd.ExecuteNonQuery()
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+
+            cmd.CommandText = "INSERT INTO `json_data`(`json`) VALUES ('" & jsondata & "')"
+            DR.Close()
+            Try
+                cmd.ExecuteNonQuery()
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+            conn.Close()
+        Else
+            cmd.CommandText = "INSERT INTO `json_data`(`json`) VALUES ('" & jsondata & "')"
+            DR.Close()
+            Try
+                cmd.ExecuteNonQuery()
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+            conn.Close()
+        End If
+    End Sub
+End Class
